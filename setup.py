85c25b821e775ae4a5e00c97a6c60ca82f8367a7
bleutooth65 <31289560+zmerino@users.noreply.github.com>
2024-04-25 17:59:29 -0400

    updated maintainer

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
 	version='3.0',
 	author='Dmitri Iouchtchenko',
 	author_email='diouchtc@uwaterloo.ca',
-	maintainer='Stephen Harrigan',
-	maintainer_email='srharrig@uwaterloo.ca',
+	maintainer='Zach Merino',
+	maintainer_email='zmerinog@uwaterloo.ca',
 	description='Package for interfacing with devices and building user '
 			'interfaces.',
 	license='BSD',
